6f1e64374b3dfe23ca1c125e12e63f579245e7c3
adrianol4cerda <adrianodeolacerda@gmail.com>
2025-05-19 16:19:52 -0300

    Update database.py
    adição das variáveis relativas aos novos formulários

--- src/database.py
+++ src/database.py
@@ -41,7 +41,7 @@ async def get_user_db(
 
 class Patient(Base):
     __tablename__ = 'patient'
-
+#AMPI-AB
     id: Mapped[int] = mapped_column(primary_key=True)
     full_name: Mapped[str]
     social_name: Mapped[str] = mapped_column(nullable=True)
@@ -66,3 +66,47 @@ class Patient(Base):
     abandono: Mapped[str]
     observacoes: Mapped[str] = mapped_column(nullable=True)
     points: Mapped[int]
+#SNELLEN (VISÃO)
+    snellen: Mapped[str] = mapped_column(nullabe=True)
+#SUSSURRO (AUDIÇÃO)
+    sussurro: Mapped[str] = mapped_column(nullable=True)
+#KATZ (INDEPENDENCIA)
+    banho: Mapped[str] = mapped_column(nullable=True)
+    vestir: Mapped[str] = mapped_column(nullable=True)
+    banheiro: Mapped[str] = mapped_column(nullable=True)
+    transferencia: Mapped[str] = mapped_column(nullable=True)
+    continencia: Mapped[str] = mapped_column(nullable=True)
+    alimentacao: Mapped[str] = mapped_column(nullable=True)
+#LAWTON (INDEPENDENCIA)
+    telefone: Mapped[str] = mapped_column(nullable=True)
+    mobilidade: Mapped[str] = mapped_column(nullable=True)
+    compras: Mapped[str] = mapped_column(nullable=True)
+    cozinhar: Mapped[str] = mapped_column(nullable=True)
+    limpar: Mapped[str] = mapped_column(nullable=True)
+    reparos: Mapped[str] = mapped_column(nullable=True)
+    lavar_roupas: Mapped[str] = mapped_column(nullable=True)
+    tomar_remedio: Mapped[str] = mapped_column(nullable=True)
+    financas: Mapped[str] = mapped_column(nullable=True)
+#MARCHA (VELOCIDADE)
+    primeira_med: Mapped[float] = mapped_column(nullable=True)
+    segunda_med: Mapped[float] = mapped_column(nullable=True)
+    terceira_med: Mapped[float] = mapped_column(nullable=True)
+    vel_marcha: Mapped[float] = mapped_column(nullable=True)
+#DEPRESSÃO GERIÁTRICA
+    satisfacao: Mapped[str] = mapped_column(nullable=True)
+    atividade: Mapped[str] = mapped_column(nullable=True)
+    felicidade: Mapped[str] = mapped_column(nullable=True)
+    casa: Mapped[str] = mapped_column(nullable=True)
+    pontuacao: Mapped[int] = mapped_column(nullable=True)
+#COGNIÇÃO
+    dia_do_mes: Mapped[str] = mapped_column(nullable=True)
+    mes: Mapped[str] = mapped_column(nullable=True)
+    ano: Mapped[str] = mapped_column(nullable=True)
+    memoria: Mapped[str] = mapped_column(nullable=True)
+    fluencia: Mapped[str] = mapped_column(nullable=True)
+    ajuste_escolaridade: Mapped[str] = mapped_column(nullable=True)
+    soma_cognicao: Mapped[int] = mapped_column(nullable=True)
+
+
+
+
